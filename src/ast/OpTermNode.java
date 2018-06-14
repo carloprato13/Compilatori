6a9f043353e991f06085f71ba89482c1e083b493
luca-Serena <lucaserena35@gmail.com>
2018-06-14 16:10:41 +0200

    Add files via upload

--- src/ast/OpTermNode.java
+++ src/ast/OpTermNode.java
@@ -8,54 +8,69 @@ import lib.FOOLlib;
 
 public class OpTermNode implements Node {
 
-  private Node left;
-  private Node right;
-  private String op;
-  
-  public OpTermNode (Node l, Node r, String ops) {
-    left=l;
-    right=r;
-    op= ops;
-  }
-  
-  @Override
-	public ArrayList<SemanticError> checkSemantics(Environment env) {
-	  //create the result
-	  ArrayList<SemanticError> res = new ArrayList<SemanticError>();
-	  
-	  //check semantics in the left and in the right exp
-	  
-	  res.addAll(left.checkSemantics(env));
-	  res.addAll(right.checkSemantics(env));
-	  
-	  return res;
-	}
-  
-  public String toPrint(String s) {
-    return s+"Mult\n" + left.toPrint(s+"  ")  
-                      + right.toPrint(s+"  ") ; 
-  }
-  
-  public Node typeCheck() {
-    if (! ( FOOLlib.isSubtype(left.typeCheck(),new IntTypeNode()) &&
-            FOOLlib.isSubtype(right.typeCheck(),new IntTypeNode()) ) ) {
-      System.out.println("Non integers in multiplication");
-      System.exit(0);
-    }
-    return new IntTypeNode();
-  }  
-  
-  public String codeGeneration() {
-		return left.codeGeneration()+
-			   right.codeGeneration()+
-			   "mult\n";
-  }
+    private Node left;
+    private Node right;
+    private String op;
 
-    public boolean isSubTypeOf(Node m){
-        if(m instanceof IntTypeNode)
-            return true;
-        else
-            return false;
-    } 
-  
-}  
+    public OpTermNode(Node l, Node r, String ops) {
+        left = l;
+        right = r;
+        op = ops;
+    }
+
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        //create the result
+        ArrayList<SemanticError> res = new ArrayList<SemanticError>();
+
+        //check semantics in the left and in the right exp
+        res.addAll(left.checkSemantics(env));
+        res.addAll(right.checkSemantics(env));
+
+        return res;
+    }
+
+    public String toPrint(String s) {
+        switch (op) {
+
+            case "+":
+                return s + "Add\n" + left.toPrint(s + "  ")
+                        + right.toPrint(s + "  ");
+
+            case "-":
+                return s + "Sub\n" + left.toPrint(s + "  ")
+                        + right.toPrint(s + "  ");
+
+            case "/":
+                return s + "Div\n" + left.toPrint(s + "  ")
+                        + right.toPrint(s + "  ");
+
+            case "*":
+                return s + "Mult\n" + left.toPrint(s + "  ")
+                        + right.toPrint(s + "  ");
+            default: return null;
+        }
+    }
+
+    public Node typeCheck() {
+        if (!(FOOLlib.isSubtype(left.typeCheck(), new IntTypeNode())
+                && FOOLlib.isSubtype(right.typeCheck(), new IntTypeNode()))) {
+            System.out.println("Non integers in numerical operations");
+            System.exit(0);
+        }
+        return new IntTypeNode();
+    }
+
+    @Override
+    public String codeGeneration() {
+        return left.codeGeneration()
+                + right.codeGeneration()
+                + "mult\n";
+    }
+
+    @Override
+    public boolean isSubTypeOf(Node m) {
+        return m instanceof IntTypeNode;
+    }
+
+}
